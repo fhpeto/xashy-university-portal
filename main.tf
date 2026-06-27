0cbaf0771d9a16eb227503d15b8fe2a55379644f
Fomo Peto <fpeto@terpmail.umd.edu>
2026-06-27 11:34:05 -0400

    update instance sg rules

--- main.tf
+++ main.tf
@@ -20,6 +20,22 @@ module "vpc" {
 resource "aws_security_group" "web_app_sg" {
   name   = "${var.project}-web-app-sg"
   vpc_id = module.vpc.vpc_id
+
+  ingress {
+    description = "Allow HTTP inbound"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 
